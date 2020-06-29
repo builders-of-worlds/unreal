6da44f0a8ac9e78f600dc4d3ce33cf0647e73032
kissgabor3843 <67091441+kissgabor3843@users.noreply.github.com>
2020-06-29 15:58:58 +0200

    mmm

---
+++ Tower_Defense/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+
+            Debug.Log("Hello");
+
+        }
+
+
+    }
+}
