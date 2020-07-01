fc0d634b3554c95d7912c276137d4bf37adee861
Dósa István <dosaistvan158@gmail.com>
2020-07-01 20:47:05 +0200

    Add Spot and connect with turret

---
+++ Tower_Defense/Assets/Scripts/PlaceTower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower : MonoBehaviour
+{
+
+
+    public GameObject towerPrefab;
+    private GameObject tower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool CanPlaceTower()
+    {
+        return tower == null;
+    }
+
+    void OnMouseUp()
+    {
+        
+        if (CanPlaceTower())
+        {            
+            tower = (GameObject)Instantiate(towerPrefab, transform.position, Quaternion.identity);                       
+        }
+    }
+}
