72f27570f35ba4255aedec0bba7ba4e5c0c8075a
Oglenor <dosaistvan158@gmail.com>
2020-07-02 15:59:41 +0200

    modify tower

--- Tower_Defense/Assets/Scripts/Tower/Tower.cs
+++ Tower_Defense/Assets/Scripts/Tower/Tower.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
 
+    public double hp;
 
+    public double Hp { get { return this.hp; } set { this.hp = value; } }
 
     // Start is called before the first frame update
     void Start()
